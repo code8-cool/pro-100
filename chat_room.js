cb0a2a66fd34430c902e3c2fcf5d8144310a600a
code8-cool <136864069+code8-cool@users.noreply.github.com>
2023-11-27 19:43:22 -0800

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,24 @@
+function addRoom ()
+{
+room_name = document.getElementById("room_name").value;
+firebase.database().ref("/").child(room_name).update({
+purpose : "adding room name"
+});
+
+localStorage.setItem("room_name", room_name);
+
+window.location = "chat_room.html";
+} 
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+  Room_names = childKey;
+ row = "<div class='room_name' id = " + Room_names + " onclick = 'redirectToRoomName'>" + Room_names +"</div><hr>";
+ document.getElementById("output").innerHTML += row;
+ });});}
+getData();
+
+function redirectToRoomName()
+{
+  localStorage.setItem("room_name", room_name);
+    window.location = "chat_room.html";
+}
